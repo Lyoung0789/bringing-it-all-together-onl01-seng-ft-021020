f8a4b6a559072b77e84112e4ac79ab81ec170e11
Lester Young <lyoung070789@gmail.com>
2020-03-12 23:23:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -104,7 +104,9 @@ def self.find_or_create_by(name:, breed:)
   if !dog.empty?
     new_dog = self.new(dogs)
   else 
-    new_dog = self.create(:name => name, :breed => breed)
+    new_dog = self.create({:name => name, :breed => breed})
+  end 
+  new_dog
 
   
 end 
