9508263592748879ad2d92a549976ae940d904fc
Lester Young <lyoung070789@gmail.com>
2020-03-12 23:45:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -104,7 +104,7 @@ def self.find_or_create_by(name:, breed:)
   if !dogs.empty?
     dog_data = dogs[0]
     binding.pry 
-    new_dog = self.new(dogs)
+    new_dog = self.new(dog_data[0], dog_data[1], dog_data[2])
   else 
     new_dog = self.create({:name => name, :breed => breed})
   end 
