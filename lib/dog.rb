ab42047abc7713480c8815f445b0eea81e63ae2f
Lester Young <lyoung070789@gmail.com>
2020-03-12 22:26:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -30,8 +30,8 @@ class Dog
     new_dog = self.new
     binding.pry
     new_dog.id = row[0]
-    new_dog[:name] = row[1]
-    new_dog.[:breed] = row[2]
+    new_dog.name = row[1]
+    new_dog.breed = row[2]
     new_dog
   
   
