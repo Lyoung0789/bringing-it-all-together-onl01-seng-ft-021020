deed64772e3f5ec6fbaa5de7fe4c8942ec0390de
Lester Young <lyoung070789@gmail.com>
2020-03-12 23:18:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -99,7 +99,7 @@ def self.find_or_create_by(hash)
     WHERE name = ? AND breed = ?
   SQL
   binding.pry
-  dogs = DB[:conn].execute(sql, hash.name)
+  dogs = DB[:conn].execute(sql, name:, breed:)
   binding.pry 
 
   
