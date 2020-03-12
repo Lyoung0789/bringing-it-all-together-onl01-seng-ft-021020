cd9d226bdefa2f0fbabaf35c33fe3afc2b7a069c
Lester Young <lyoung070789@gmail.com>
2020-03-12 23:20:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -99,7 +99,7 @@ def self.find_or_create_by(name:, breed:)
     WHERE name = ? AND breed = ?
   SQL
   # binding.pry
-  dogs = DB[:conn].execute(sql, name, breed)
+  dogs = DB[:conn].execute(sql, name, breed).first
   binding.pry 
 
   
