043b1c5bea40b5ce50899d694c14e2aa2394dc3b
Lester Young <lyoung070789@gmail.com>
2020-03-12 22:32:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,15 +27,12 @@ class Dog
   
   
   def self.new_from_db (row)
-    
+    binding.pry
     new_dog = self.new(id: row[0], name: row[1], breed: row[2])
-    
     new_dog.id= row[0]
-    
     new_dog
-  
-  
   end 
+  
   def save
   end 
   
